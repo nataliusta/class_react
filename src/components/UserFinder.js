3e7d41cae44d1967ce680a7ba5383aa10e4481d0
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-08-24 20:41:52 +0300

    Create class component UserFinder

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -1,4 +1,4 @@
-import { Fragment, useState, useEffect } from 'react';
+import { Fragment, useState, useEffect, Component } from 'react';
 
 import Users from './Users';
 import classes from './UserFinder.module.css';
@@ -9,7 +9,42 @@ const DUMMY_USERS = [
   { id: 'u3', name: 'Julie' },
 ];
 
-const UserFinder = () => {
+class UserFinder extends Component {
+    constructor() {
+        super();
+        this.state = {
+            filteredUsers: DUMMY_USERS,
+            searchTerm: ''
+        };
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.searchTerm !== this.state.searchTerm) {
+            this.setState({
+                filteredUsers: DUMMY_USERS.filter((user) => 
+                    user.name.includes(this.state.searchTerm)
+                ),
+            });
+        }
+        
+    }
+
+    searchChangeHandler(event) {
+        this.setState({searchTerm: event.target.value});
+    }
+    render() {
+        return (
+            <Fragment>
+              <div className={classes.finder}>
+                <input type='search' onChange={this.searchChangeHandler.bind(this)} />
+              </div>
+              <Users users={this.state.filteredUsers} />
+            </Fragment>
+        );
+    }
+}
+
+/*const UserFinder = () => {
   const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
   const [searchTerm, setSearchTerm] = useState('');
 
@@ -31,6 +66,6 @@ const UserFinder = () => {
       <Users users={filteredUsers} />
     </Fragment>
   );
-};
+};*/
 
 export default UserFinder;
